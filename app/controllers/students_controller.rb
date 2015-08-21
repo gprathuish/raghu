506dca0b85f2b181b3dfccdee5476da1f3f01e39
gprathuish <gprathuis@gmail.com>
2015-08-21 19:47:48 +0530

    Prathuihs - Added Edit, New, Show functions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,39 @@
 class StudentsController < ApplicationController
+	before_filter :authenticate 
+	before_action :set_student, only: [:edit, :show, :update]
 		layout 'view'
 	def index
 		redirect_to new_user_session_path unless current_user.present?
-		@users = Student.all
+		@students = Student.all
 	end
+
+	def new
+		@student = Student.new
+	end
+
+	def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      redirect_to @student      
+    else
+      render :new      
+    end
+  end
 
 	def show
 		@user = Student.find(params[:id])
+	end
+
+	def edit
+	end
+
+	def update
+		if @student.update(student_params)		
+			redirect_to students_path
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
@@ -14,4 +41,17 @@ class StudentsController < ApplicationController
 		@student.delete
 		redirect_to students_path
 	end
+
+	private
+		def set_student
+			@student = Student.find(params[:id])
+		end
+
+		def student_params						
+			params.require("student").permit(:name,:email,:phone,:dob,:city,:country)
+		end
+
+		def authenticate
+			redirect_to new_user_session_path unless current_user.present?
+		end
 end
